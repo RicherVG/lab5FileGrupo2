f4a8a75f5dea9ee226f60a481b8c19f108e8f0ee
herminiosauceda0314 <micuentadosparami@gmail.com>
2026-02-27 09:33:27 -0600

    Subida Clase

---
+++ cmd/src/cmd/ResultadoComando.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cmd;
+
+/**
+ *
+ * @author hermi
+ */
+public class ResultadoComando {
+    private String mensaje;
+    private boolean entrar;
+    private boolean salir;
+    
+    public ResultadoComando(String mensaje, boolean entrar, boolean salir){
+        this.mensaje=mensaje;
+        this.entrar=entrar;
+        this.salir=salir;
+    }
+    
+    public static ResultadoComando normal(String msg){
+        return new ResultadoComando(msg,false,false);
+    }
+    
+    public static ResultadoComando entrarModo(String msg){
+        return new ResultadoComando(msg,false,false);
+    }
+    
+    public static ResultadoComando salirModo(String msg){
+        return new ResultadoComando(msg,false,true);
+    }
+    
+    public String getMensaje(){
+        return mensaje;
+    }
+    
+    public boolean isEntrar(){
+        return entrar;
+    }
+    
+    public boolean isSalir(){
+        return salir;
+    }
+}
